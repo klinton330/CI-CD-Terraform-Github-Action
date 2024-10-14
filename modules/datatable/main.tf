80b3c75fdf94efd5f9a51529665f03e155d90ee4
KLINTON J <klintonece@gmail.com>
2024-10-14 19:57:24 +0530

    updated description

--- modules/datatable/main.tf
+++ modules/datatable/main.tf
@@ -29,6 +29,6 @@ resource "genesyscloud_architect_datatable" "Test_Terraform" {
     type    = "boolean"
     default = "false"
   }
-  description = "Datatable for Terraform 1"
+  description = "Datatable for Terraform latest"
 }
 
